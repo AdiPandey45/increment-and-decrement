1601f9740086ce5f13aa774a9977fc86833f2b15
Adi Pandey <135324527+AdiPandey45@users.noreply.github.com>
2025-01-13 23:48:49 +0530

    Create inc and dec.js

---
+++ inc and dec.js
@@ -0,0 +1,12 @@
+const counter =document.querySelector('#counter');
+const increment= () =>{
+    let value=parseInt(counter.innerText);
+    value=value+1;
+    counter.innerText=value;
+}
+
+const decrement = ()=>{
+    let value=parseInt(counter.innerText);
+    value=value-1;
+    counter.innerText=value;
+}
