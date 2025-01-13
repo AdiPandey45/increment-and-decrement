1ca0d363b3a975afed8463aedfdf2b3e54c6c11a
Adi Pandey <135324527+AdiPandey45@users.noreply.github.com>
2025-01-13 23:51:00 +0530

    Update inc and dec.js

--- inc and dec.js
+++ inc and dec.js
@@ -3,7 +3,7 @@ const increment= () =>{
     let value=parseInt(counter.innerText);
     value=value+1;
     counter.innerText=value;
-}
+} 
 
 const decrement = ()=>{
     let value=parseInt(counter.innerText);
